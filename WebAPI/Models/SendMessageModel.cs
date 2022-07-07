93d3689af4af04c9723d597798dc31fbf2c277b9
Victor Latriglia <victor_latriglia@epam.com>
2022-07-07 14:39:39 -0500

    Changing model names to comply with Meta, I'll deal with Sonar later

--- WebAPI/Models/SendMessageModel.cs
+++ WebAPI/Models/SendMessageModel.cs
@@ -3,12 +3,12 @@ public class SendMessageModel
 {
     public readonly string messaging_product = "whatsapp";
     public readonly string recipient_type = "individual";
-    public string To { get; set; }
+    public string to { get; set; }
     public readonly string type = "text";
     public readonly WhatsappTextModel text;
     public SendMessageModel(string to, string body)
     {
-        this.To = to;
+        this.to = to;
         this.text = new WhatsappTextModel(body);
     }
 }
@@ -16,9 +16,9 @@ public class SendMessageModel
 public class WhatsappTextModel
 {
     public readonly bool preview_url = false;
-    public string Body { get; set; }
+    public string body { get; set; }
     public WhatsappTextModel(string body)
     {
-        this.Body = body;
+        this.body = body;
     }
 }
