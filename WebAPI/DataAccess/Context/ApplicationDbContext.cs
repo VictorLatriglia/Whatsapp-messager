109f746968b0b7b62f640526f18bb7e8afd9acca
Victor Latriglia <victor_latriglia@epam.com>
2022-07-08 22:56:08 -0500

    updated DB schema and ensuring db creation

--- WebAPI/DataAccess/Context/ApplicationDbContext.cs
+++ WebAPI/DataAccess/Context/ApplicationDbContext.cs
@@ -5,8 +5,13 @@ namespace Whatsapp_bot.DataAccess.Context;
 public class ApplicationDbContext : DbContext, IDbContext
 {
     public DbSet<Log> Logs { get; set; }
+    public DbSet<OutgoingsCategory> OutgoingsCategories { get; set; }
+    public DbSet<OutgoingsTag> OutgoingsTags { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserOutgoing> UserOutgoings { get; set; }
+
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> opts) : base(opts)
     {
-
+        Database.EnsureCreated();
     }
 }
